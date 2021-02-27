0ef0324d892f302bd4279baa425fc234fee53074
BodNS <bondarenokn@gmail.com>
2021-02-27 10:23:57 +0200

    clear file

--- assets/view_model/view_model.js
+++ assets/view_model/view_model.js
@@ -1,8 +1,1 @@
 'use strict';
-//import {sum, sub, mult,div} from '../model/model'; //perviy sposob
-import * as calcul from '../model/model.js';
-console.log(calcul.sum(2,2));
-console.log(calcul.sub(2,2));
-console.log(calcul.mult(2,2));
-console.log(calcul.div(2,2));
-console.log(calcul);
